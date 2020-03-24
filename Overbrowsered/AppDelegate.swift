9ada2fe2665f0329e47d71eb0da6738998f9edf9
Brian Misiak <bmisiak@me.com>
2020-03-23 23:27:58 -0700

    Handle opening .html files

--- Overbrowsered/AppDelegate.swift
+++ Overbrowsered/AppDelegate.swift
@@ -23,7 +23,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 	func applicationWillFinishLaunching(_ notification: Notification) {
 		
 		// Load the saved most recently used browser...
-		
 		let savedMostRecentBrowserBundleUrl = UserDefaults.standard.url(forKey: "mostRecentBrowserBundleUrl")
 		
 		if let url = savedMostRecentBrowserBundleUrl {
@@ -34,20 +33,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 		// running, sorted by the time of last use: https://gist.github.com/0xced/163918
 		// The API is deprecated though.
 		
-		// If there's no valid saved browser, try using the one currently set as default...
-		
+		// If there's no valid saved browser, try using the one currently set as default:
 		if self.mostRecentlyUsedBrowser == nil {
-			
 			if let defaultBrowserUrl = NSWorkspace.shared.urlForApplication(toOpen: URL(string: "https://example.com")!) {
 				
 				let defaultBrowserBundle = Bundle(url: defaultBrowserUrl)
-				// But only if the default handler is not this-very-app.
+				// But only if the default handler is not Overbrowsered.
 				if defaultBrowserBundle != Bundle.main {
 					self.mostRecentlyUsedBrowser = defaultBrowserBundle
 				}
-				
 			}
-			
 		}
 		
 		// Let's let the OS know we want to handle http links. Note: this has to
@@ -78,8 +73,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 	
 	public func menuNeedsUpdate(_ menu: NSMenu) {
 		menu.removeAllItems()
+        
+        menu.addItem(withTitle: "Overbrowsered by @bmisiak", action: nil, keyEquivalent: "")
+        menu.addItem(NSMenuItem.separator())
 		
-		//Most recent browser status
 		menu.addItem(withTitle: "Most recently used browser: \(self.mostRecentlyUsedBrowser?.infoDictionary?["CFBundleName"] as? String ?? "Unknown (use any browser to detect)")", action: nil, keyEquivalent: "")
 		
 		//Detect the default handler status
@@ -90,12 +87,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 		
 		if defaultBrowserBundle == Bundle.main {
 			menu.addItem(withTitle: "Default http handler: me 👌", action: nil, keyEquivalent: "")
+        } else if defaultBrowserBundle?.bundleIdentifier == Bundle.main.bundleIdentifier {
+            menu.addItem(withTitle: "Default http handler: Overbrowsered", action: nil, keyEquivalent: "")
 		} else {
-			menu.addItem(withTitle: "Default http handler: \(defaultBrowserBundle?.infoDictionary?["CFBundleName"] as? String ?? defaultBrowserBundle?.bundleIdentifier ?? "not me") ☹️", action: nil, keyEquivalent: "")
-			menu.addItem(withTitle: "⚠️ For this app to work, click here to set it as the default \"browser\".", action: #selector(self.menuBarSetDefault(_:)), keyEquivalent: "")
+            let handlerName = defaultBrowserBundle?.infoDictionary?["CFBundleName"] as? String ?? defaultBrowserBundle?.bundleIdentifier
+            
+            menu.addItem(withTitle: "Default http handler: \(handlerName ?? "not me") ☹️", action: nil, keyEquivalent: "")
+            menu.addItem(withTitle: "⚠️ For Overbrowsered to work, click here to set it as the default \"browser\".", action: #selector(self.menuBarSetDefault(_:)), keyEquivalent: "")
 		}
 		
-		//Other items
 		menu.addItem(NSMenuItem.separator())
 		menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
 	}
@@ -103,6 +103,16 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 	// Try setting this app as the default handler for http(s), per user request:
 	
 	@objc func menuBarSetDefault(_ sender: Any?) {
+        guard Bundle.main.bundlePath.starts(with: "/Applications/") else {
+            let alert = NSAlert.init()
+            alert.alertStyle = .informational
+            alert.addButton(withTitle: "OK")
+            alert.messageText = "Please move Overbrowsered to your Applications folder."
+            alert.informativeText = "You will run into issues if you move Overbrowsered after setting it as the default browser. Please move it into Applications beforehand."
+            alert.runModal()
+            return
+        }
+        
 		if let bundleId = Bundle.main.bundleIdentifier {
 			LSSetDefaultHandlerForURLScheme("http" as CFString, bundleId as CFString)
 			LSSetDefaultHandlerForURLScheme("https" as CFString, bundleId as CFString)
@@ -121,26 +131,28 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 			return
 		}
 		
-		var appIsABrowser: Bool = false
+        let appIsABrowser =
+            (appBundle.infoDictionary?["CFBundleURLTypes"] as? [[String:Any?]])?
+            .map { $0["CFBundleURLSchemes"] as? [String?] }
+            .contains { schemes in schemes?.contains { $0 == "http" || $0 == "https" } ?? false }
+            ?? false
 		
-		let supportedUrlTypes = appBundle.infoDictionary?["CFBundleURLTypes"] as? [[String:Any?]]
-		
-		if let supportedUrlTypes = supportedUrlTypes {
-			for supportedUrl in supportedUrlTypes {
-				let schemes = supportedUrl["CFBundleURLSchemes"] as? [String?]
-				
-				schemes?.forEach { scheme in
-					if scheme == "http" || scheme == "https" {
-						appIsABrowser = true
-					}
-				}
-			}
-		}
-		
-		if(appIsABrowser) {
+		if appIsABrowser {
 			self.mostRecentlyUsedBrowser = appBundle
 		}
 	}
+    
+    func application(_ sender: NSApplication, openFiles filenames: [String]) {
+        if let browserBundleId = mostRecentlyUsedBrowser?.bundleIdentifier {
+            NSWorkspace.shared.open(
+                filenames.map { URL.init(fileURLWithPath: $0).standardized },
+                withAppBundleIdentifier: browserBundleId,
+                options: .default,
+                additionalEventParamDescriptor: nil,
+                launchIdentifiers: nil
+            )
+        }
+    }
 	
 	@objc func handleHttpLink(getUrl: NSAppleEventDescriptor, withReplyEvent: NSAppleEventDescriptor) {
 		
@@ -148,20 +160,15 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 		guard let url = URL(string: urlStr) else { return }
 		
 		if let browserBundleId = mostRecentlyUsedBrowser?.bundleIdentifier {
-
 			NSWorkspace.shared.open([url], withAppBundleIdentifier: browserBundleId, options: .default, additionalEventParamDescriptor: nil, launchIdentifiers: nil)
-
 		} else {
-
 			let alert = NSAlert.init()
 			alert.alertStyle = .informational
 			alert.addButton(withTitle: "OK")
 			alert.messageText = "Overbrowsered has yet to see you use a browser."
 			alert.informativeText = "Open a web browser and click its window, so I can know where to open this link:\n\n\(urlStr)"
 			alert.runModal()
-
 		}
-		
 	}
 
 }
